061f26c1564dc87fa8640ad0a4add33ed13f4634
leandrokanis <leandro.alves@g44.com.br>
2018-11-05 11:09:27 -0200

    1006

--- 1-iniciante/1006.js
+++ 1-iniciante/1006.js
@@ -2,16 +2,23 @@ const raw_input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = raw_input.split('\n');
 const input = () => parseFloat(lines.shift()) ;
 
-const sum = (x,y) => {
-  return (x+y);
+const calc_media = (a, b, c) => {
+  let a_med = (a * 0.2);
+  let b_med = (b * 0.3);
+  let c_med = (c * 0.5);
+  return(
+    (a_med + b_med + c_med).toFixed(1)
+  );
+
 };
 
 const main = () => {
   const a = input();
   const b = input();
+  const c = input();
 
   return (
-    'X = ' + sum(a, b)
+    'MEDIA = ' + calc_media(a, b, c)
   );
 
 };
