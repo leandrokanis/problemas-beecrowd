79f076bf7fe162d033a508d660eedeb3a92e503c
leandrokanis <leandro.alves@g44.com.br>
2018-11-05 23:32:57 -0200

    let to const

--- 1-iniciante/1006.js
+++ 1-iniciante/1006.js
@@ -3,9 +3,9 @@ const lines = raw_input.split('\n');
 const input = () => parseFloat(lines.shift()) ;
 
 const calc_media = (a, b, c) => {
-  let a_med = (a * 0.2);
-  let b_med = (b * 0.3);
-  let c_med = (c * 0.5);
+  const a_med = (a * 0.2);
+  const b_med = (b * 0.3);
+  const c_med = (c * 0.5);
   return(
     (a_med + b_med + c_med).toFixed(1)
   );
