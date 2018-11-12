15ec6466295e79439ed0a946af6f39b8e7e1ff4e
Leandro Alves <leandrosustenido@gmail.com>
2018-11-11 23:45:36 -0200

    1011

---
+++ 1-iniciante/1011.js
@@ -0,0 +1,18 @@
+const raw_input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = raw_input.split('\n');
+const input = () => parseFloat(lines.shift()) ;
+
+const volume = (r) => {
+  const pi =  3.14159;
+  return ( (4.0/3.0) * pi * r * r * r ).toFixed(3);
+};
+
+const main = () => {
+
+  return (
+    'VOLUME = ' + volume(input())
+  );
+
+};
+
+console.log(main());
