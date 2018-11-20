64046256bc8adb6f59d0426620cb8dd936bc37ef
Leandro Alves <leandrosustenido@gmail.com>
2018-11-20 10:59:12 -0200

    adding multiple input, to split multiple values in a single line

--- templates/template.js
+++ templates/template.js
@@ -1,6 +1,7 @@
 const raw_input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = raw_input.split('\n');
 const input = () => parseFloat(lines.shift()) ;
+const multiple_input = () => input().split(" ").map(i => parseFloat(i));
 
 const sum = (x,y) => {
   return (x+y);
