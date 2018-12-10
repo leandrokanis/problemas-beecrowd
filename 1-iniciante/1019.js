25ca6306ab975ec612653933123eba4ad05a9e44
Leandro Alves <leandrosustenido@gmail.com>
2018-12-10 12:12:09 -0200

    1019

--- 1-iniciante/1019.js
+++ 1-iniciante/1019.js
@@ -1,20 +1,25 @@
 const raw_input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = raw_input.split('\n');
-const input = () => parseFloat(lines.shift()) ;
+const input = () => parseInt(lines.shift()) ;
 const multiple_input = () => input().split(" ").map(i => parseFloat(i));
 
-const sum = (x,y) => {
-  return (x+y);
+const horas = (x) => {
+  return parseInt(x/3600);
+};
+
+const minutos = (x) => {
+  return parseInt(x/60)%60;
+};
+
+const segundos = (x) => {
+  return (x%60)%60;
 };
 
 const main = () => {
   const a = input();
-  const b = input();
-
-  return (
-    'X = ' + sum(a, b)
-  );
+  console.log(horas(a) + ":" + minutos(a) + ":" + segundos(a));
+  return true;
 
 };
 
-console.log(main());
+main();
